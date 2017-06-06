7d9c4e3106ab3bd3a7c67af572ed28dd84e45fa0
Nontchai Tantiponsawas <nontchai@Nontchais-MacBook-Pro.local>
2017-06-06 11:51:24 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
-import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
+import ReactMapboxGl, { Layer, Feature, Marker, Popup, GeoJSONLayer, Source, Cluster } from "react-mapbox-gl";
+import pin from './bed.png'
+import marker from './park.png'
+import style from './index.css'
+import post from './post.png'
 
 class App extends Component {
   constructor(props){
@@ -11,11 +15,26 @@ class App extends Component {
     console.log(event)
   }
   render() {
+    const box = {
+      position: 'relative'
+    }
+    const popup = {
+      position: 'absolute',
+      'text-align': 'center',
+      background: '#fff',
+      margin: '0px',
+      padding: '2px 5px',
+      'border-radius': '2px',
+      'font-size': '1.2em'
+    }
+    const image = {
+      width: '30px',
+      height: '30px'
+    }
     const bounds = [
 	[98.21264043965243, 7.730684939826489],
 	[98.48870852324956, 8.222634324830736] 
     ]
-    console.log(bounds)
     return (
       <div>
         <ReactMapboxGl
@@ -28,15 +47,62 @@ class App extends Component {
           }}
           movingMethod="jumpTo"
           onClick= {this.clickMap.bind(this)}
-          zoom= {[12]}
+          zoom= {[13]}
           maxBounds= {bounds}
         >
-        <Layer
-	    type="symbol"
-	    id="marker"
-	    layout={{ "icon-image": "harbor-15" }}>
-	    <Feature coordinates={[98.3981, 7.89059]}/>
-	  </Layer> 
+      {/*<Layer
+	  type="symbol"
+	  id="marker"
+	  layout={{ "icon-image": "marker-15" }}>
+	  <Feature coordinates={[98.34073603045908, 7.952391285598864]}/>
+	</Layer>*/}
+        <Cluster ClusterMarkerFactory={this.clusterMarker} clusterThreshold={8}>
+            <Marker
+	      coordinates={[98.34073603045908, 7.952391285598864]}
+	      anchor="bottom">
+              <div style={box}>
+                <p className='popup'>21:00</p>
+                <img style={image} src={pin} />
+              </div>
+	    </Marker>
+	    <Marker
+		coordinates={[98.31276113132395, 8.125716936541309]}
+		anchor="bottom">
+	      <div style={box}>
+		<p className='popup'>21:00</p>
+		<img style={image} src={marker} />
+	      </div>
+	    </Marker>
+	    <Marker
+		coordinates={[98.39006019462784, 8.049915585699637]}
+		anchor="bottom">
+	      <div style={box}>
+		<p className='popup'>21:00</p>
+		<img style={image} src={post} />
+	      </div>
+	    </Marker>
+          </Cluster>
+        {/* <Marker
+	    coordinates={[98.34073603045908, 7.952391285598864]}
+	    anchor="bottom">
+            <img src={pin} />
+	</Marker>
+        <Marker
+	    coordinates={[98.31276113132395, 8.125716936541309]}
+	    anchor="bottom">
+          <div style={box}>
+            <p className='popup'>21:00</p>
+            <img style={image} src={marker} />
+          </div>
+	</Marker>
+        <Marker
+	    coordinates={[98.39006019462784, 8.049915585699637]}
+	    anchor="bottom">
+          <div style={box}>
+            <p className='popup'>21:00</p>
+            <img style={image} src={post} />
+          </div>
+	</Marker>*/}
 	</ReactMapboxGl>
       </div>
     );
